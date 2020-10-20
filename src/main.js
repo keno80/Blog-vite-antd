523565e4e1dd15858dcad7cff5d618150a3f5656
keno80 <1211582428@qq.com>
2020-10-20 23:34:48 +0800

    添加MarkDown预览工具

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
 import components from "./utils/antd";
+import VMdEditor from "@kangc/v-md-editor/lib/preview";
+import '@kangc/v-md-editor/lib/style/preview.css'
+import VuePressTheme from '@kangc/v-md-editor/lib/theme/vuepress'
 
-createApp(App).use(router).use(components).mount('#app')
+VMdEditor.use(VuePressTheme)
+
+createApp(App).use(router).use(components).use(VMdEditor).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 export default {
   optimizeDeps: {
-    include: ['@ant-design/colors']
+    include: ['@ant-design/colors', '@kangc/v-md-editor/lib/preview.js', '@kangc/v-md-editor/lib/theme/vuepress.js']
   }
 }
